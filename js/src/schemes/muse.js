3e6500d16b47d5165c791f43bd6c6f4299f4042b
李留住 <liliuzhu@renrenche.com>
2019-03-04 11:21:52 +0800

    Site updated: 2019-03-04 11:21:51

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,9 +1,3 @@
-$(document).ready(function() {
-  function updateFooterPosition() {
-    var containerHeight = $('#footer').attr('position') ? $('.container').height() + $('#footer').outerHeight(true) : $('.container').height();
-    if (containerHeight < window.innerHeight) $('#footer').css({ 'position': 'fixed', 'bottom': 0, 'left': 0, 'right': 0 }).attr('position', 'fixed');
-    else $('#footer').removeAttr('style position');
-  }
-  updateFooterPosition();
-  $(window).on('resize scroll', updateFooterPosition);
-});
+// build time:Mon Mar 04 2019 11:21:47 GMT+0800 (中国标准时间)
+$(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
+//rebuild by neat 

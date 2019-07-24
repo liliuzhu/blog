fb86444320c22741cc7a60e5879c3ce0c492dea4
李留住 <liliuzhu@renrenche.com>
2019-07-24 15:52:03 +0800

    Site updated: 2019-07-24 15:52:01

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Thu Jun 27 2019 20:40:35 GMT+0800 (GMT+08:00)
+// build time:Wed Jul 24 2019 15:51:54 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 

d1d86795a1889aebae13661fb69b370bc4abff73
李留住 <liliuzhu@renrenche.com>
2019-10-23 17:22:56 +0800

    Site updated: 2019-10-23 17:22:53

--- js/schemes/pisces.js
+++ js/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Tue Jul 30 2019 16:22:16 GMT+0800 (GMT+08:00)
+// build time:Wed Oct 23 2019 17:22:44 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var t=$(".sidebar-inner");var i=CONFIG.sidebar.offset||12;function e(){return $(".header-inner").height()+i}function a(){var t=$("#footer");var i=$(".footer-inner");var e=t.outerHeight()-i.outerHeight();var a=t.outerHeight()+e;return a}function r(){var r=e();var o=a();var f=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight();var n=$("#content").height();if(r+f<n){t.affix({offset:{top:r-i,bottom:o}});t.affix("checkPosition")}$("#sidebar").css({"margin-top":r,"margin-left":"auto"})}function o(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");r()}function f(){var t=window.matchMedia("(min-width: 992px)");t.addListener(function(t){if(t.matches){o()}})}r();f()});
 //rebuild by neat 
